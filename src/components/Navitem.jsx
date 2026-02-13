eacdeacbfc17a6f0947e398780eef0297d96d21b
axilfum7 <muflixarahmatullayeva@gmail.com>
2026-02-13 12:44:20 +0500

    hero done

--- src/components/Navitem.jsx
+++ src/components/Navitem.jsx
@@ -3,7 +3,7 @@ const Navitem = ({ item }) => {
     return (
         <a className='relative text-gray-700 hover:text-[#8BAC3E]' href={item.path}>
             {item.name}
-            {item.badge (
+            {item.badge && (
         <span className="absolute -right-3 -top-3 text-[8px] px-2 py-1 bg-[#E7462D] text-white rounded-md">
           {item.badge}
         </span>
--- src/modules/Hero.jsx
+++ src/modules/Hero.jsx
@@ -1,15 +1,41 @@
+import {HeroImg} from "../assets/images"
 import { Button } from "../components"
 
 const Hero = () => {
-    return (
-        <section>
-            <div className="containers">
-                <Button title={"Daftar Sekarang"} />
-                <Button extraClass={`!bg-[#F2F2F2] !text-black`} title={"About Us"} />
+  return (
+    <section className="bg-[#F9FFF5] py-20">
+      <div className="containers flex flex-col lg:flex-row items-center justify-between gap-12">
+        <div className="max-w-xl">
+          <h1 className="text-4xl lg:text-6xl font-bold text-[#8BAC3E] leading-tight">
+            Good Food Us <br /> Good Mood
+          </h1>
 
-            </div>
-        </section>
-    )
+          <p className="text-gray-500 mt-6 mb-8 leading-relaxed">
+            I would think that conserving our natural resources should be a conservative position:
+            Not to waste food, and not to throw away a lot of the food that we buy.
+          </p>
+
+          <div className="flex gap-4">
+            <Button title="Daftar Sekarang" />
+            <Button
+              title="About Us"
+              extraClass="!bg-[#F2F2F2] !text-black"
+            />
+          </div>
+        </div>
+        <div className="relative">
+          <img
+            src={HeroImg}
+            alt="Food"
+            className="w-112.5 lg:w-137.5"
+          />
+          
+        </div>
+
+
+      </div>
+    </section>
+  )
 }
 
 export default Hero
